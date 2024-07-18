9630d9bb741c3171d14e97eeb8a72fc542399ad0
leeyewon81 <leeyewon81@gmail.com>
2024-07-18 19:29:05 +0900

    123

--- FinalProject/src/main/java/shop/model/ShopBean.java
+++ FinalProject/src/main/java/shop/model/ShopBean.java
@@ -80,7 +80,7 @@ public class ShopBean {
 		this.review_count = review_count;
 	}
 	public int getCategory_id() {
-		return category_id;
+		return category_id; 
 	}
 	public void setCategory_id(int category_id) {
 		this.category_id = category_id;
